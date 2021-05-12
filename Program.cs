c256c2a8bc7917d7a2f1b9751f4e3e06705bfa35
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-12 14:02:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LambdaPractice
+{
+    public delegate int AddNumbers(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers del = (a, b) => a + b;
+            
+            Console.WriteLine(del(1,2));
+        }
+    }
+}
